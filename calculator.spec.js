41bf2dcef32972bc0dc87542f25751535cb60406
Paul-Ngigi <paulkush7777@gmail.com>
2022-07-11 14:18:24 +0300

    add test cases to the calculator function

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,9 +1,28 @@
 describe("Calculator.js", function () {
-  it("should add number to the total", function () {});
+  it("should add number to the total", function () {
+    const calculator = new Calculator();
+    calculator.add(5);
+    expect(calculator.total).toBe(5);
+  });
 
-  it("should subtract number to the total", function () {});
+  it("should subtract number to the total", function () {
+    const calculator = new Calculator();
+    calculator.total = 10;
+    calculator.substract(4);
+    expect(calculator.total).toBe(6);
+  });
 
-  it("should multiply number to the total", function () {});
+  it("should multiply number to the total", function () {
+    const calculator = new Calculator();
+    calculator.total = 10;
+    calculator.multiply(4);
+    expect(calculator.total).toBe(40);
+  });
 
-  it("should divide number to the total", function () {});
+  it("should divide number to the total", function () {
+    const calculator = new Calculator();
+    calculator.total = 10;
+    calculator.divide(4);
+    expect(calculator.total).toBe(2.5);
+  });
 });
