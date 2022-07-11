1048e8c3ae09595ead3a0903ac73d4399428c787
Paul-Ngigi <paulkush7777@gmail.com>
2022-07-11 13:55:49 +0300

    define the test cases to be ran

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,3 +1,9 @@
 describe("Calculator.js", function () {
+  it("should add number to the total", function () {});
 
+  it("should subtract number to the total", function () {});
+
+  it("should multiply number to the total", function () {});
+
+  it("should divide number to the total", function () {});
 });
